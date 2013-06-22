970259d23aa61904c6d492b3f98841844fc6b8fe
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-06-22 18:27:13 +0900

    write #size, #fib; passed

--- fib_interval.rb
+++ fib_interval.rb
@@ -1,10 +1,25 @@
 class FibInterval
-  attr_reader :size
+  attr_reader :size, :fib
 
-  def initialize(n)
-    @size = n
+  SIZE_MIN = 4
+
+  def initialize(size)
+    if size.is_a? Integer
+      raise "'size' required at least #{SIZE_MIN}" if size < SIZE_MIN
+      @size = size
+      @size.freeze
+      @fib = generate_fib(size)
+      @fib.freeze
+    else
+      raise "argument class error"
+    end
   end
 
-  def fib
+  private
+  def generate_fib(n) # [1, 2, 3, 5, 8, 13, 21, ...]
+    ary = []
+    x,y = 1,1
+    n.times { ary << y; x, y = y, x+y }
+    ary
   end
 end
--- spec/fib_interval_spec.rb
+++ spec/fib_interval_spec.rb
@@ -1,10 +1,21 @@
 require_relative '../fib_interval'
 
 describe FibInterval do
-  context "construct with/without arg" do
+  context "when constructing" do
+    describe do
+      it { expect(FibInterval::SIZE_MIN).to be >= 4 }
+    end
+
     describe "#new" do
       it { expect { FibInterval.new }.to raise_error }
       it { expect { FibInterval.new(10) }.not_to raise_error }
+      it { expect { FibInterval.new(FibInterval::SIZE_MIN) }.not_to raise_error }
+
+      it { expect { FibInterval.new(nil) }.to raise_error }
+      it { expect { FibInterval.new('10') }.to raise_error }
+      it { expect { FibInterval.new(-1) }.to raise_error }
+      it { expect { FibInterval.new(0) }.to raise_error }
+      it { expect { FibInterval.new(1) }.to raise_error }
     end
   end
 
@@ -13,12 +24,29 @@ describe FibInterval do
     subject { FibInterval.new(n) }
 
     describe "#size" do
-      it { expect(subject.size).to eq(n) }
+      it { expect(subject.size).to eq n }
     end
 
     describe "#fib" do
-      let(:x) { subject.fib }
-      it { expect(x).to be_a_kind_of(Array) }
+      let(:fib) { subject.fib }
+      it { expect(fib).to be_a_kind_of Array }
+      it { expect(fib.size).to be >= subject.size }
+      it { expect(fib.first 2).to eq [1,2] }
+      it {
+        expect {
+          result = true
+          a,b = 1,2
+          fib[2 .. -1].each { |x|
+            if (a+b) == x
+              a,b = b,x
+            else
+              result = false
+              break
+            end
+          }
+          result
+        }.to be_true
+      }
     end
   end
 end
