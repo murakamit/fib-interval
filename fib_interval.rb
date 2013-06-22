b883e4c8df79bf8b81e0944dec8b840fa05e696e
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-06-22 17:30:26 +0900

    learning RSpec syntax; rspec => '...F'

--- fib_interval.rb
+++ fib_interval.rb
@@ -1,2 +1,10 @@
 class FibInterval
+  attr_reader :size
+
+  def initialize(n)
+    @size = n
+  end
+
+  def fib
+  end
 end
--- spec/fib_interval_spec.rb
+++ spec/fib_interval_spec.rb
@@ -1,4 +1,24 @@
 require_relative '../fib_interval'
 
 describe FibInterval do
+  context "construct with/without arg" do
+    describe "#new" do
+      it { expect { FibInterval.new }.to raise_error }
+      it { expect { FibInterval.new(10) }.not_to raise_error }
+    end
+  end
+
+  context "constructed successfully" do
+    let(:n) { 10 }
+    subject { FibInterval.new(n) }
+
+    describe "#size" do
+      it { expect(subject.size).to eq(n) }
+    end
+
+    describe "#fib" do
+      let(:x) { subject.fib }
+      it { expect(x).to be_a_kind_of(Array) }
+    end
+  end
 end
