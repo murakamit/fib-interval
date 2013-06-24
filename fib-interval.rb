2d00f464e4088ff7b77a69a365b18c3e6e845455
MURAKAMI Teiji <murakami.teiji@gmail.com>
2013-06-24 18:07:28 +0900

    give up virtual-delte; return an index simply; call Nth as needed; update spec; not coding yet

--- fib-interval.rb
+++ fib-interval.rb
@@ -99,7 +99,6 @@ class FibInterval
     if idx > 0
       ivals[idx - 1] += ivals[idx]
       ivals.delete_at(idx)
-      # ivals[idx] = nil
     else
       ivals.shift
     end
--- spec/fib-interval_spec.rb
+++ spec/fib-interval_spec.rb
@@ -49,68 +49,66 @@ describe FibInterval do
       end
     end
 
-    describe "#indexes_to_delete" do
+    describe "#index_to_delete" do
       [ [], [0], [0,0], [1], [1,1], [1,1,0], [3,2,1] ].each { | intervals |
         it {
-          expect { subject.indexes_to_delete(intervals) }.not_to raise_error
+          expect { subject.index_to_delete(intervals) }.not_to raise_error
         }
       }
     end
 
-    context "when #indexes_to_delete receive Not-a-Positive-Integer Array" do
+    context "when #index_to_delete receive Not-a-Positive-Integer Array" do
       [nil, "", 0, %w(1 1 1), [0.0], [-1] ].each { |x|
-        it { expect { subject.indexes_to_delete(x) }.to raise_error }
+        it { expect { subject.index_to_delete(x) }.to raise_error }
       }
     end
 
-    context "when #indexes_to_delete receive ascending intervals" do
+    context "when #index_to_delete receive ascending intervals" do
       [ [1, 2, 3], [1, 0, 2], [2, 1, 2] ].each { | intervals |
-        it { expect { subject.indexes_to_delete(intervals) }.not_to raise_error }
+        it { expect { subject.index_to_delete(intervals) }.not_to raise_error }
       }
     end
 
-    describe "#indexes_to_delete don't destroy arg" do
+    describe "#index_to_delete don't destroy arg" do
       [0, 1, 2, 3, 10, 100].each { |n|
         it do
           intervals = Array.new(n, 1)
           intervals.freeze
-          expect { subject.indexes_to_delete intervals }.not_to raise_error
+          expect { subject.index_to_delete intervals }.not_to raise_error
           expect(intervals.size).to eq n
         end
       }
     end
 
-    describe "#indexes_to_delete example" do
+    describe "#index_to_delete example" do
       context "intervals.size < holding_capacity" do
-        let(:ret) { subject.indexes_to_delete [] }
-        it { expect(ret).to be_a_kind_of Array }
-        it { expect(ret).to be_empty }
+        it { expect(subject.index_to_delete []).to be_nil }
       end
 
       context "intervals.size == holding_capacity" do
         # holding_capacity   == 6
         # intervals_capacity == 5
         [
-         [ [0, 0, 0, 0, 0], [0] ],
-         [ [1, 1, 1, 1, 1], [1] ],
-         [ [2, 1, 1, 1, 1], [1] ],
-         [ [3, 1, 1, 1, 1], [2] ],
-         [ [3, 2, 1, 1, 1], [1] ],
-         [ [5, 1, 1, 1, 1], [2] ],
-         [ [5, 2, 1, 1, 1], [2] ],
-         [ [5, 3, 1, 1, 1], [3] ],
-         [ [5, 3, 2, 1, 1], [1] ],
-         [ [8, 2, 1, 1, 1], [2] ],
-         [ [8, 3, 1, 1, 1], [3] ],
-         [ [8, 3, 2, 1, 1], [2] ],
-         [ [8, 5, 1, 1, 1], [3] ],
-         [ [8, 5, 2, 1, 1], [3] ],
-         [ [8, 5, 3, 1, 1], [4] ],
-         [ [8, 5, 3, 2, 1], [0] ],
+         [ [0, 0, 0, 0, 0], 0 ],
+         [ [1, 1, 1, 1, 1], 1 ],
+         [ [2, 1, 1, 1, 1], 1 ],
+         [ [3, 1, 1, 1, 1], 2 ],
+         [ [3, 2, 1, 1, 1], 1 ],
+         [ [5, 1, 1, 1, 1], 2 ],
+         [ [5, 2, 1, 1, 1], 2 ],
+         [ [5, 3, 1, 1, 1], 3 ],
+         [ [5, 3, 2, 1, 1], 1 ],
+         [ [8, 2, 1, 1, 1], 2 ],
+         [ [8, 3, 1, 1, 1], 3 ],
+         [ [8, 3, 2, 1, 1], 2 ],
+         [ [8, 5, 1, 1, 1], 3 ],
+         [ [8, 5, 2, 1, 1], 3 ],
+         [ [8, 5, 3, 1, 1], 4 ],
+         [ [8, 5, 3, 2, 1], 0 ],
         ].each { | ab |
           it {
             a, b = ab
-            expect(subject.indexes_to_delete a).to eq b
+            expect(subject.index_to_delete a).to eq b
           }
         }
       end
@@ -120,25 +118,13 @@ describe FibInterval do
         # intervals_capacity == 5
         let(:valid_full_intervals) { [8, 5, 3, 2, 1] }
         let(:delta) { 10 }
-        let(:ary0) { Array.new(delta, 0) }
+        let(:ary1) { Array.new(delta, 1) }
 
         it {
-          intervals2 = valid_full_intervals + ary0
-          dels = subject.indexes_to_delete intervals2
-          expect(dels).to eq dels.uniq
+          intervals2 = valid_full_intervals + ary1
+          i = subject.index_to_delete intervals2
+          expect(i).to eq valid_full_intervals.size
         }
-
-
-        # it "holds the newest" do
-        #   delta = 100
-        #   intervals2 = valid_full_intervals + Array.new(delta, 0)
-        #   dels = subject.indexes_to_delete intervals2
-        #   a = []
-        #   delta.times { |i| a << (valid_full_intervals.size + i) }
-        #   a.unshift(valid_full_intervals.size - 1)
-        #   a.pop
-        #   expect(dels).to eq a
-        # end
       end
 
       context "when intervals contain irregular 0" do
@@ -155,19 +141,18 @@ describe FibInterval do
          [2, 0, 1, 0, 1],
          [2, 1, 2, 0, 1],
         ].each { | intervals |
-          it { expect { subject.indexes_to_delete intervals }.not_to raise_error }
+          it { expect { subject.index_to_delete intervals }.not_to raise_error }
         }
       end
-
 
       context "when intervals contain non-fib" do
         # holding_capacity   == 6
         # intervals_capacity == 5
         [
-         [ [8, 5, 4, 1, 1], [2] ],
+         [ [8, 5, 4, 1, 1], 2 ],
         ].each { | ab |
           a, b = ab
-          it { expect(subject.indexes_to_delete a).to eq b }
+          it { expect(subject.index_to_delete a).to eq b }
         }
       end
     end
